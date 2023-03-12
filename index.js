ac0a092f59407fd9b9ba032d85cfd2f7a550ad2c
Leroy Scott Jr <leroysjr@yahoo.com>
2023-03-11 19:09:36 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+names = ["Guadalupe", "Ollie", "Aki"];
+
+
+
+function writeCards (names,event) {
+    let messages = [];
+for( let i = 0 ; i < names.length; i++) {
+messages.push("Thank you, " + names[i] + ", for the wonderful surprise gift!")
+}
+return messages;
+};
+
+function countDown (number){
+    while (number >=0) {
+        console.log(number);
+        number --;
+    };
+};
+console.log(writeCards);
+console.log(countDown());
+
